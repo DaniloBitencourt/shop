bc9b36806c4bdedb5e8f949d7ed4a361a5968998
Danilo Bitencourt <danilo300600@gmail.com>
2024-06-03 17:14:11 -0300

    Criando a visualização de cada Product Item

---
+++ lib/components/product_item.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+  const ProductItem({super.key, required Product this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: Colors.black54,
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.favorite),
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart),
+          ),
+        ),
+      ),
+    );
+  }
+}
