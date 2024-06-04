e75e70ce333883380adba194784517abbf21cd64
Danilo Bitencourt <danilo300600@gmail.com>
2024-06-04 14:21:03 -0300

    Adicionando um PopUp Menu para utilizar uma filtragem de produtos por favoritos

--- lib/screen/products_overview_screen.dart
+++ lib/screen/products_overview_screen.dart
@@ -2,22 +2,58 @@ import 'package:flutter/material.dart';
 
 import '../components/product_grid.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
-  
+enum FilterOptions{
+  Favorite,
+  All,
+}
 
+class ProductsOverviewScreen extends StatefulWidget {
   const ProductsOverviewScreen({super.key});
 
+  @override
+  State<ProductsOverviewScreen> createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  
+  bool _showFavoriteOnly = false;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Minha Loja',
-          style: TextStyle(color: Theme.of(context).colorScheme.tertiary),
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.tertiary,
+          ),
         ),
+        actions: [
+          PopupMenuButton(
+            icon: Icon(Icons.more_vert,color: Colors.white,),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Somente Favoritos'),
+                value: FilterOptions.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.All,
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue){
+              setState(() {
+                if(selectedValue == FilterOptions.Favorite){
+                  _showFavoriteOnly = true;
+                }else{
+                  _showFavoriteOnly = false;
+                }
+              });
+            },
+          )
+        ],
       ),
-      body: ProductGrid(),
+      body: ProductGrid(_showFavoriteOnly),
     );
   }
 }
-
