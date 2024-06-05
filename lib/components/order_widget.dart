2e3c6914c0e02b00d35ce90fab6b8aa3e59bad35
Danilo Bitencourt <danilo300600@gmail.com>
2024-06-05 17:53:15 -0300

    Terminando a tela de pedidos com a opção de ver detalhes de cada Pedido

--- lib/components/order_widget.dart
+++ lib/components/order_widget.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:shop/models/order.dart';
 
-class OrderWidget extends StatelessWidget {
+class OrderWidget extends StatefulWidget {
   final Order order;
   const OrderWidget({required this.order, super.key});
+
+  @override
+  State<OrderWidget> createState() => _OrderWidgetState();
+}
+
+class _OrderWidgetState extends State<OrderWidget> {
+  bool _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -13,23 +20,62 @@ class OrderWidget extends StatelessWidget {
         horizontal: 15,
         vertical: 4,
       ),
-      child: ListTile(
-        leading: CircleAvatar(
-            child: Padding(
-              padding: const EdgeInsets.all(5),
-              child: FittedBox(
-                child: Text(order.total.toStringAsFixed(2)),
+      child: Column(
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: FittedBox(
+                  child: Text(widget.order.total.toStringAsFixed(2)),
+                ),
               ),
             ),
+            title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
+            subtitle: Text(
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date),
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
           ),
-        title: Text('R\$ ${order.total.toStringAsFixed(2)}'),
-        subtitle: Text(
-          DateFormat('dd/MM/yyyy hh:mm').format(order.date),
-        ),
-        trailing: IconButton(
-          icon: Icon(Icons.expand_more),
-          onPressed: (){},
-        ),
+          if (_expanded)
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 4,
+            ),
+            height: (widget.order.products.length * 25.0) + 10,
+            child: ListView(
+              children: widget.order.products.map((product) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      product.name,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '${product.quantity}x R\$ ${product.price}',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey,
+                      ),
+                    )
+                  ],
+                );
+              }).toList(),
+            ),
+          ),
+        ],
       ),
     );
   }
