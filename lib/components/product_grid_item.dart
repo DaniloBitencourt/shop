478c2e0e981b2bfc936404f689c6d58bcdc7adfa
Danilo Bitencourt <danilo300600@gmail.com>
2024-06-06 15:37:51 -0300

    Adicionando a opção de desfazer ação de por produto no carrinho e um dialog para verificar se o usuário tem certeza na ação de remover produto do carrinho

--- lib/components/product_grid_item.dart
+++ lib/components/product_grid_item.dart
@@ -5,7 +5,7 @@ import '../models/cart.dart';
 import '../models/product.dart';
 import '../utils/app_routes.dart';
 
-class ProductItem extends StatelessWidget {
+class ProductGridItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
@@ -43,6 +43,19 @@ class ProductItem extends StatelessWidget {
           ),
           trailing: IconButton(
             onPressed: () {
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Produto adicionado com sucesso!'),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'DESFAZER',
+                    onPressed: (){
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
               cart.addItem(product);
             },
             icon: const Icon(Icons.shopping_cart),
