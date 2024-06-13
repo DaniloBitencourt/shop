9fd1536424b552b7ee73c634479c6227ee6d4d9e
Danilo Bitencourt <danilo300600@gmail.com>
2024-06-13 14:24:49 -0300

    Corrigindo a função de remoção de produtos para remover do banco de dados também

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/exceptions/http_exception.dart';
 import 'package:shop/models/product.dart';
 import 'package:shop/utils/app_routes.dart';
 
@@ -11,6 +12,7 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final msg = ScaffoldMessenger.of(context);
     return ListTile(
       leading: CircleAvatar(
         backgroundImage: NetworkImage(product.imageUrl),
@@ -32,29 +34,38 @@ class ProductItem extends StatelessWidget {
             ),
             IconButton(
               onPressed: () {
-                showDialog(
+                showDialog<bool>(
                   context: context,
                   builder: (ctx) => AlertDialog(
                     title: const Text('Tem Certeza?'),
                     content: const Text('Quer remover o produto?'),
                     actions: [
                       TextButton(
-                        onPressed: () {
-                          Navigator.of(ctx).pop();
-                        },
+                        onPressed: () => Navigator.of(ctx).pop(false),
                         child: const Text('Não'),
                       ),
                       TextButton(
-                        onPressed: () {
-                          Provider.of<ProductList>(context, listen: false)
-                              .removeProduct(product);
-                          Navigator.of(ctx).pop();
-                        },
+                        onPressed: () => Navigator.of(ctx).pop(true),
                         child: const Text('Sim'),
                       ),
                     ],
                   ),
-                );
+                ).then((value) async {
+                  if (value ?? false) {
+                    try {
+                      await Provider.of<ProductList>(
+                        context,
+                        listen: false,
+                      ).removeProduct(product);
+                    } on HttpException catch (error) {
+                      msg.showSnackBar(
+                        SnackBar(
+                          content: Text(error.toString()),
+                        ),
+                      );
+                    }
+                  }
+                });
               },
               icon: const Icon(Icons.delete),
               color: Theme.of(context).colorScheme.error,
