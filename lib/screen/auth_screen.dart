1938410f1746853d2141058783406599c69f0a8a
Danilo Bitencourt <danilo300600@gmail.com>
2024-06-15 00:11:23 -0300

    Resolvendo o erro de overflow quando o teclado é aberto

--- lib/screen/auth_screen.dart
+++ lib/screen/auth_screen.dart
@@ -11,9 +11,6 @@ class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Tela de Autentificação'),
-      ),
       body: Stack(
         children: [
           Container(
@@ -28,45 +25,44 @@ class AuthScreen extends StatelessWidget {
               ),
             ),
           ),
-          Center(
-            child: SingleChildScrollView(
-              child: Container(
-                width: double.infinity,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(bottom: 20),
-                      padding: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 70,
-                      ),
-                      //cascade operator ..
-                      transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.deepOrange.shade900,
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 8,
-                            color: Colors.black26,
-                            offset: Offset(0, 2),
-                          ),
-                        ]
-                      ),
-                      child: Text(
-                        'Minha Loja',
-                        style: TextStyle(
-                          fontSize: 45,
-                          fontFamily: 'Anton',
-                          color: Theme.of(context).colorScheme.tertiary,
+          SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(top: 200),
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 70,
+                    ),
+                    //cascade operator ..
+                    transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.deepOrange.shade900,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 8,
+                          color: Colors.black26,
+                          offset: Offset(0, 2),
                         ),
+                      ]
+                    ),
+                    child: Text(
+                      'Minha Loja',
+                      style: TextStyle(
+                        fontSize: 45,
+                        fontFamily: 'Anton',
+                        color: Theme.of(context).colorScheme.tertiary,
                       ),
                     ),
-                    AuthForm(),
-                  ],
-                ),
+                  ),
+                  AuthForm(),
+                ],
               ),
             ),
           ),
